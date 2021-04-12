f6d2ca848b115ef526ac924f0f5f1b5849278357
Andhana Utama <magicwarms@gmail.com>
2021-04-12 19:59:43 +0700

    refactor(delete all files): delete all files

--- src/ErrorHandler.ts
+++
@@ -1,5 +0,0 @@
-export default class ErrorHandler extends Error {
-    constructor(public success: Boolean, public statusCode: number, public message: string) {
-        super();
-    }
-}
--- src/tasks/TaskController.ts
+++
@@ -1,9 +0,0 @@
-class TaskController {
-    defaultMethod() {
-        return {
-            text: `You've reached the ${this.constructor.name} default method`,
-        };
-    }
-}
-
-export = new TaskController();
